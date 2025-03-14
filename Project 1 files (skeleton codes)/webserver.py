d106f181fcf63debe7dfd7d4f9966ca8f4c9712c
Malachi-H <malachi.haskew@icloud.com>
2025-03-15 00:07:42 +1100

    Added a few comments
    - Added comments explaining the message processing which is just normal python (nothing really network related)
    - Also Added a few questions for pleaces that confuse me and I haven't looked into yet.

--- Project 1 files (skeleton codes)/webserver.py
+++ Project 1 files (skeleton codes)/webserver.py
@@ -1,11 +1,11 @@
 # import socket module
-from socket import *
+from socket import * # type: ignore
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 # Prepare a sever socket
 
 # Fill in start
-serverPort = 8080
+serverPort = 8080 # Does the port number here matter as long as we are have the same number in our localhost url (http://localhost:8080)? IDK
 serverSocket.bind(("", serverPort))
 serverSocket.listen(1)
 # Fill in end
@@ -17,12 +17,33 @@ while True:
         serverSocket.accept()
     )  # Fill in start              #Fill in end
     try:
+        # What URL do we use to ge to the html file? (http://localhost:8080/simpleWeb.html doesn't work) IDK
         message = connectionSocket.recv(
-            1024
-        ).decode()  # Fill in start          #Fill in end
+            1024 # Why 1024? IDK
+        ).decode() # What is it decoding from Binary? IDK
+        # message looks something like the following:
+        '''
+        GET /simpleweb.html HTTP/1.1​
+        Host: localhost:6789​
+        User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0​
+        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8​
+        Accept-Language: en-US,en;q=0.5​
+        Accept-Encoding: gzip, deflate, br​
+        DNT: 1​
+        Connection: keep-alive​
+        Upgrade-Insecure-Requests: 1​
+        Sec-Fetch-Dest: document​
+        Sec-Fetch-Mode: navigate​
+        Sec-Fetch-Site: none​
+        Sec-Fetch-User: ?1​ 
+        '''
         filename = message.split()[1]
+        # calling split and getting the second element gets the file name (url path) that the client is requesting.
+        # in this case it's "/simpleweb.html"
         f = open(filename[1:])
-        outputdata = f.read()  # Fill in start       #Fill in end
+        # open's the file (removes the slash) (using open with would have been better I think)
+        outputdata = f.read()
+        # reads the locally stored simpleweb.html file. It was part of the skeleton code
 
         # Send one HTTP header line into socket
         connectionSocket.send(
